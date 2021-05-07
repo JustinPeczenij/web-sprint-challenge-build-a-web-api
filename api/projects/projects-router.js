9f03794187f7dffe01a6cc58ae405ffdd4e6c266
Justin Peczenij <justinpeczenij@gmail.com>
2021-05-07 06:38:00 -0400

    server is functional

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,10 @@
 // Write your "projects" router here!
+const router = require('express').Router()
+const Projects = require('./projects-model')
+
+router.get('/', (req, res) => {
+    console.log('working')
+    res.send('pow')
+})
+
+module.exports = router
